0177bb88857ef3300ba7e0c3eda2b1749c6b321b
NBESS <nicholasdbess@gmail.com>
2020-10-05 09:56:17 -0400

    complete initial server setup

--- server.js
+++ server.js
@@ -6,5 +6,22 @@ const morgan = require('morgan');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(errorhandler());
+app.use(cors());
+app.use(morgan('dev'));
+
+
+
+app.listen(PORT, () => console.log(`Listening: Port ${PORT}`));
+
+module.exports = app;
+
+
+
+
+
 
 
