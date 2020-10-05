b55033c67957a51d8cf7e635a24c812fd4677198
NBESS <nicholasdbess@gmail.com>
2020-10-05 09:48:32 -0400

    add server.js and dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+
+const app = express();
+
+
+
