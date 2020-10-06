653c5d7420af5f7c0b317507fef29fba42c9b629
NBESS <nicholasdbess@gmail.com>
2020-10-06 08:50:34 -0400

    Add post/put routes in series router

--- api/series.js
+++ api/series.js
@@ -35,4 +35,53 @@ seriesRouter.get('/:seriesId', (req, res, next) => {
     res.status(200).json({ series: req.series });
 })
 
+const validateSeries = (req, res, next) => {
+    const seriesItem = req.body.series;
+    if (!seriesItem.name || !seriesItem.description) {
+        return res.sendStatus(400);
+    }
+    next();
+}
+
+seriesRouter.post('/', validateSeries, (req, res, next) => {
+    const seriesItem = req.body.series;
+    const sql = `INSERT INTO Series (name, description) VALUES ($name, $description)`;
+    const values = {
+        $name: seriesItem.name,
+        $description: seriesItem.description
+    };
+
+    db.run(sql, values, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            db.get(`SELECT * FROM Series WHERE id = ${this.lastID}`, (err, series) => {
+                res.status(201).json({ series: series })
+            })
+        }
+    })
+})
+
+seriesRouter.put('/:seriesId', validateSeries, (req, res, next) => {
+    const seriesItem = req.body.series;
+    const sql = `UPDATE Series SET name = $name, description = $description WHERE id = $seriesId`;
+    const values = {
+        $name: seriesItem.name,
+        $description: seriesItem.description,
+        $seriesId: req.params.seriesId
+    };
+
+    db.run(sql, values, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            db.get(`SELECT * FROM Series WHERE id = ${req.params.seriesId}`, (err, updatedSeries) => {
+                res.status(200).json({ series: updatedSeries });
+            })
+        }
+    })
+})
+
+
+
 module.exports = seriesRouter;
